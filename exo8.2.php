1e00b8842c91b977c34bbf1059bb68056e7b3d2e
Ph4ks <62299825+Ph4ks@users.noreply.github.com>
2020-03-18 09:31:31 +0100

    Create exo8.2.php

---
+++ exo8.2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+<?php
+
+$nombre = 8;
+$facteur = 0;
+echo "Table de $nombre:<br>";
+    for ($facteur >=0; $facteur <=10 ; $facteur++) {
+    $produit = $facteur*$nombre;
+    echo "$facteur x $nombre = $produit<br>";
+    
+    }
+
+    ?>
+
+
