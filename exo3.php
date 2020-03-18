27830a49fe5a6aace541476ef3f7aca4c4e71468
Ph4ks <62299825+Ph4ks@users.noreply.github.com>
2020-03-18 09:29:43 +0100

    Create exo3.php

---
+++ exo3.php
@@ -0,0 +1,12 @@
+
+<html>
+<body>
+<?php
+        $oldtxt="Notre formation DL commence aujourd'hui.;
+        $newtxt=str_replace("aujourd'hui", "demain", "$oldtxt");
+    echo "L'ancienne phrase est $oldtxt <br/>";
+    echo "La nouvelle phrase est $newtxt";
+    ?>
+</body>
+</html>
+
