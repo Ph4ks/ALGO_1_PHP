429fd8c7a1d45cd71f7fa7a2cc497bc16bed2526
Ph4ks <62299825+Ph4ks@users.noreply.github.com>
2020-03-18 09:30:40 +0100

    Create exo7.php

---
+++ exo7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+<html>
+<?php
+
+$age = 4; 
+        if ($age <= 5 ) {
+            echo" L'enfant ne fait partie d'aucune categorie";
+        }
+        elseif(6 <= $age && $age<= 7) {
+        echo "L'enfant qui a $age ans appartient a la categorie 'Minime'";
+        }
+        elseif (8 <= $age && $age <= 9) {
+        echo "L'enfant qui a $age ans appartient a la categorie 'Pupille'";
+        }
+        elseif (10 <= $age && $age<= 11){
+        echo "L'enfant qui a $age ans appartient a la categorie 'Pupille'";
+        }
+        elseif (12 <= $age){
+        echo "L'enfant qui a $age ans appartient a la categorie 'Pupille'";
+        }
+        
+        
+        ?>
