3686894237bba6582f068bfc4fdbb8bd2a95100f
Ph4ks <62299825+Ph4ks@users.noreply.github.com>
2020-03-18 09:29:21 +0100

    Create exo2.php

---
+++ exo2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$phrase =("Notre formation DL commence aujourd'hui");
+$longueur=("Notre formation DL commence aujourd'hui");
+    $str=str_word_count($longueur);
+    echo "La phrase « $phrase » contient $str mots."
+?>
+</body>
+</html>
+
