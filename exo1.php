9e2d50a472c0cc13462ae930ee7a8b64c820b331
Ph4ks <62299825+Ph4ks@users.noreply.github.com>
2020-03-18 09:28:55 +0100

    Create exo1.php

---
+++ exo1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$phrase =("Notre formation DL commence aujourd'hui");
+$longueur=("Notre formation DL commence aujourd'hui");
+    $str=strlen($longueur);
+    echo "La phrase « $phrase » contient $str caracteres."
+?>
+</body>
+</html>
+
